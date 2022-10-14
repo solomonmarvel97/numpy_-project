0946f6fad35e9dd8508f06f1b70d210bd7a18012
Marvelous <solomonmarvel@hotmail.com>
2022-10-14 13:00:58 +0100

    added requirements for creating a simple flask endpoint

---
+++ app.py
@@ -0,0 +1,17 @@
+def check_server(ip: str):
+	pass
+
+	
+from flask import Flask, render_template, url_for, request
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'GET':
+        return "Hello"
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
